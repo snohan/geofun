9cd546f5a724f2ffae2fc6c9ee8147b8b64254e0
Snorre Hansen <snorre.hansen@vegvesen.no>
2021-06-12 23:22:00 +0200

    Include Hamar, knit directly to index.html

--- platonic.R
+++ platonic.R
@@ -1,7 +1,8 @@
-# A Platonic Cache
+# Solving mysteries...
 library(tidyverse)
-#library(sf)
+library(sf)
 
+# A Platonic Cache ----
 # Must be a regular icosahedron circumscribed by a sphere, 
 # thus with its 12 vertices tangent to the sphere's surface.
 
@@ -58,3 +59,34 @@ points
 # Change to cartesian coordinates and use (0, +-1, +-phi)?
 phi <- (1 + sqrt(5)) / 2
 lat_dev <- atan(0.5) / pi * 180
+
+
+# The Large Heap ----
+calculate_decimal_minutes <- function(decimal_degrees) {
+  decimal_minutes <- round((decimal_degrees - floor(decimal_degrees)) * 60,
+                           digits = 3)
+}
+
+addition_number <- 1.287262935597079127914529663121
+multiplication_number <- 0.199912332675918863522787694816
+
+x1 <- (addition_number + sqrt(addition_number^2 - 4 * multiplication_number)) * 0.5
+x2 <- (addition_number - sqrt(addition_number^2 - 4 * multiplication_number)) * 0.5
+y1 <- addition_number - x1
+y2 <- addition_number - x2
+
+# Must be radians - convert to latlon:
+
+lat1 <- x1 * (180 / pi)
+lon1 <- y1  * (180 / pi)
+
+nullpunkt <- tibble::tibble(cache_name = c("The Large Heap"),
+                          east = c(lon1),
+                          north = c(lat1)) %>% 
+  #sf::st_as_sf(coords = c("east", "north"), crs = 32632) %>%
+  #sf::st_transform("+proj=longlat +datum=WGS84")
+  #koordinater <- sf::st_coordinates(nullpunkt) %>% 
+  #tibble::as_tibble() %>% 
+  dplyr::mutate(min_e = calculate_decimal_minutes(east),
+                min_n = calculate_decimal_minutes(north))
+
